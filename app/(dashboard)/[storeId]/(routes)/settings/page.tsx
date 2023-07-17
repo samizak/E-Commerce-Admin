53d8d7b1947797c44119b4047671818fe3330018
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-17 23:03:16 +0100

    Created Settings page

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 
-const SettingsPage = () => {
-  return <div>Settings page</div>;
+import prismadb from "@/lib/prismadb";
+
+import { SettingsForm } from "./components/settings-form";
+
+const SettingsPage = async ({ params }: { params: { storeId: string } }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 p-8 pt-6 space-y-4">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsPage;
