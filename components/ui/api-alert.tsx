de5983f9c24c9f772524b25da87521ae415fb403
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-17 23:05:52 +0100

    Created Settings page api callout

---
+++ components/ui/api-alert.tsx
@@ -0,0 +1,47 @@
+import { Copy, Server } from "lucide-react";
+import { toast } from "react-hot-toast";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Badge, BadgeProps } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+interface ApiAlertProps {
+  title: string;
+  description: string;
+  variant: "public" | "admin";
+}
+
+const textMap: Record<ApiAlertProps["variant"], string> = {
+  public: "Public",
+  admin: "Admin",
+};
+
+const variantMap: Record<ApiAlertProps["variant"], BadgeProps["variant"]> = {
+  public: "secondary",
+  admin: "destructive",
+};
+
+export const ApiAlert: React.FC<ApiAlertProps> = ({ title, description, variant = "public" }) => {
+  const onCopy = (description: string) => {
+    navigator.clipboard.writeText(description);
+    toast.success("API Route copied to clipboard.");
+  };
+
+  return (
+    <Alert>
+      <Server className="w-4 h-4" />
+      <AlertTitle className="flex items-center gap-x-2">
+        {title}
+        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+      </AlertTitle>
+      <AlertDescription className="flex items-center justify-between mt-4">
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+          {description}
+        </code>
+        <Button variant="outline" size="sm" onClick={() => onCopy(description)}>
+          <Copy className="w-4 h-4" />
+        </Button>
+      </AlertDescription>
+    </Alert>
+  );
+};
