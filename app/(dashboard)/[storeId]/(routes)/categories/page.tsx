723d47159186e53f3ed8541e5cf1ed4a4871749f
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-22 15:52:41 +0100

    Implemented Category  entity

---
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,37 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+
+import { CategoryColumn } from "./components/columns";
+import { CategoriesClient } from "./components/client";
+
+const CategoriesPage = async ({ params }: { params: { storeId: string } }) => {
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 p-8 pt-6 space-y-4">
+        <CategoriesClient data={formattedCategories} />
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesPage;
